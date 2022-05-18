33ac525e07edb1db9a0eaf169073e27adbb50a48
h-oop <99036325+h-oop@users.noreply.github.com>
2022-05-18 09:19:23 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,8 +1,8 @@
 // DEFINE YOUR FUNCTIONS HERE BASED ON THE PROVIDED CRITERIA
 
 function includes(item, array){
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] = item) {
+    for (let i = 0; i < array.length+1; i++) {
+        if (array[i] == item) {
             return true;
         } else {
             return false;
