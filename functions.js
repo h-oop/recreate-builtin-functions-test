a390aca66af979d614e21ecb21adfbefdfb7d268
h-oop <99036325+h-oop@users.noreply.github.com>
2022-05-18 08:45:50 -0600

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,1 +1,15 @@
 // DEFINE YOUR FUNCTIONS HERE BASED ON THE PROVIDED CRITERIA
+
+function includes(item){
+    for (let i = 0; i < Array.length ; i++) {
+        if (Array[i] == item) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+function indexOf() {
+
+}
